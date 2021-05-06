6f7d41074f663cf6943321959257f08fd906deb4
André Lara Amorim <andre.amorim.si@gmail.com>
2021-05-06 12:57:15 -0300

    cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,4 +1,5 @@
 import upload from "@config/upload";
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import "reflect-metadata";
@@ -9,6 +10,7 @@ import { AppError } from "@shared/errors/AppError";
 import createConnection from "@shared/infra/typeorm";
 
 import "@shared/container";
+
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 
@@ -20,6 +22,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
